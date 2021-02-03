c219ca81d954c0e2a90ff428c155bc6f7094674b
Peter <craven@gmx.net>
2021-02-03 15:32:19 +0100

    Replace tabs with spaces.

--- make-changelog.py
+++ make-changelog.py
@@ -3,40 +3,40 @@ import collections
 import re
 import urllib.request
 lines = []
-firstline = True;
+firstline = True
 changelog = urllib.request.urlopen('https://raw.githubusercontent.com/pioneerspacesim/pioneer/master/Changelog.txt').read().decode('utf-8')
 for line in changelog.splitlines():
-	if firstline:
-		lines.append((0, line.strip()))
-		firstline = False
-	elif len(line) == 0:
-		continue
-	elif line[0].isspace():
-		indent = len(line) - len(line.lstrip(' '))
-		content = re.sub(r'#(\d+)', r'<a href="https://github.com/pioneerspacesim/pioneer/issues/\1">#\1</a>', line.strip().lstrip(' *'))
-		if content:
-			lines.append((indent, content))
-	else:
-		break
+    if firstline:
+        lines.append((0, line.strip()))
+        firstline = False
+    elif len(line) == 0:
+        continue
+    elif line[0].isspace():
+        indent = len(line) - len(line.lstrip(' '))
+        content = re.sub(r'#(\d+)', r'<a href="https://github.com/pioneerspacesim/pioneer/issues/\1">#\1</a>', line.strip().lstrip(' *'))
+        if content:
+            lines.append((indent, content))
+    else:
+        break
 Group = collections.namedtuple('Group', ['name', 'sections'])
 Section = collections.namedtuple('Section', ['name', 'entries'])
 thegroup = False
 thesection = False
 for line in lines:
-	depth = line[0]
-	content = line[1]
-	if depth == 0:
-		thegroup = Group(name=content, sections=[])
-	if depth == 3:
-		thesection = Section(name=content, entries=[])
-		thegroup.sections.append(thesection)
-	if depth == 5:
-		thesection.entries.append(content)
+    depth = line[0]
+    content = line[1]
+    if depth == 0:
+        thegroup = Group(name=content, sections=[])
+    if depth == 3:
+        thesection = Section(name=content, entries=[])
+        thegroup.sections.append(thesection)
+    if depth == 5:
+        thesection.entries.append(content)
 print(f'For {thegroup.name}. <a href=\'https://github.com/pioneerspacesim/pioneer/blob/master/Changelog.txt\'>Full changelog</a>.')
 print('<ul>')
 for section in thegroup.sections:
-	print(f'<li>{section.name}<ul>')
-	for entry in section.entries:
-		print(f'<li>{entry}</li>')
-	print('</ul></li>')
+    print(f'<li>{section.name}<ul>')
+    for entry in section.entries:
+        print(f'<li>{entry}</li>')
+    print('</ul></li>')
 print('</ul>')
